4e6ae9074d372d92d2c1bb535816c48fd22c628d
FatmaAdel96 <fatmaadel9611@gmail.com>
2018-05-04 20:19:03 +0200

    Create TamyozCards.js

---
+++ src/js/TamyozCards.js
@@ -0,0 +1,11 @@
+<Slider src={require('../images/Vector Smart Object11.png')} />
+        <CarPhoneNumbers pr="الرئيسية > أرقام الهاتف" pl="الأنتقال لأارقام السيارات"/>
+        <CarsNumbers/>
+        <Bronze image_cards="57739999099"src={require('../images/Vector Smart Object copy 13.png')}/>
+        <Line/>
+        <Bronze2 image_cards="57739999099"src={require('../images/Vector Smart Object copy 13.png')}/>
+        <Line/>
+        <Bronze2 Bronze1="b" p="price1" c="CategoryName1" br="bronze_footer1" l="link1" i="image-cards1" image_cards="57739999099"src={require('../images/Vector Smart Object copy 13.png')}/>
+        <Line/>
+        <PlusNumbers/>
+        <Line/>
