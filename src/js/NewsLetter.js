98699eaeee86ca63429f6b49ab0255e4ce6f71a1
Amina <amina@github.com>
2018-05-05 23:48:45 +0200

    last version

--- src/js/NewsLetter.js
+++ src/js/NewsLetter.js
@@ -4,6 +4,7 @@ import NewsLetterParagraph from "./../js/NewsLetterParagraph";
 import NewsLetterFoter from "./../js/NewsLetterFoter";
 import './../css/newsletter.css';
 
+//component that collect the header and foter and inner paragraph in one page 
 class NewsLetter extends Component {
   render() {
     return (
--- src/js/NewsLetterHeader.js
+++ src/js/NewsLetterHeader.js
@@ -2,11 +2,14 @@ import React , {Component} from 'react';
 import  LogoArea from "./../js/LogoArea";
 import './../css/newsletter.css';
 
+//specific header to Newsletter page as its header is diffrent from the other headers
+
 class NewsLetterHeader extends Component{
 	render(){
 		return ( 
-
+			//outer div for the logo and Tamayooz image
 		<div className="div">
+		//refrence to Logo component
         <LogoArea id="logoArea" />
 		<img  id="img1" src={require("./../images/Vector Smart Object11.png")} />
 
