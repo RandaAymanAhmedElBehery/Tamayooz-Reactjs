a3018c79b11c4ae921aace30d7c40df9ba0addae
sarayaser <sarayasser_110@yahoo.com>
2018-05-05 23:29:43 +0200

    Blog, BlogDetails, into

--- src/js/Footer.js
+++ src/js/Footer.js
@@ -4,29 +4,29 @@ import TiSocialTwitter from 'react-icons/lib/ti/social-twitter';
 import FaPaperPlane from 'react-icons/lib/fa/paper-plane';
 
 class Footer extends Component {
-  render() {
-    return (
-      <footer>
+ render() {
+   return (
+     <footer>
 
-        <img src={require("./../images/footer.png")} />
+       <img src={require("./../images/footer.png")} />
 
-        <div className="logoArea" id="footerLogoArea">
-          <img src={require("./../images/logo.png")} className="logo" />
-          <label>الحقوق محفوظة<br />&copy; 2018 تميز</label>
-        </div>
+       <div className="logoArea" id="footerLogoArea">
+         <img src={require("./../images/logo.png")} className="logo" />
+         <label>الحقوق محفوظة<br />&copy; 2018 تميز</label>
+       </div>
 
-        <label>تابعنا على</label>
-        <div id="redRect"></div>
-        <ul>
-          <li><i id="facebook" > <FaFacebookSquare color="#fff" /> </i></li>
-          <li><i id="twitter" > <TiSocialTwitter color="#fff" /> </i></li>
-          <li><i id="telegram"> <FaPaperPlane color="#fff" /> </i></li>
-        </ul>
+       <label>تابعنا على</label>
+       <div id="redRect"></div>
+       <ul>
+         <li><i id="facebook" > <FaFacebookSquare color="#fff" /> </i></li>
+         <li><i id="twitter" > <TiSocialTwitter color="#fff" /> </i></li>
+         <li><i id="telegram"> <FaPaperPlane color="#fff" /> </i></li>
+       </ul>
 
-      </footer>
+     </footer>
 
-    );
-  }
+   );
+ }
 }
 
 export default Footer;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,20 @@ import { BrowserRouter, Route} from 'react-router-dom'
 import NewsLetter from './../js/NewsLetter';
 import NewsLetterHeader from './../js/NewsLetterHeader'
 import profile from './../js/profile';
+import into from './../js/into';
+import blogDetails from './../js/blogDetails';
+import blog from './../js/blog';
+
 
 export default ()=> (
 		<BrowserRouter>
 			<div>
 				<Route path="/NewsLetter" component ={NewsLetter}/>
 				<Route path="/profile" component={profile}/>
+				<Route path="/into" component={into}/>
+				<Route path="/blogDetails" component={blogDetails}/>
+				<Route path="/blog" component={blog}/>
+
 			</div>
 		</BrowserRouter>
 );
