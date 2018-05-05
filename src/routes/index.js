974a92f2d43908134292a3c8159646a82ef825f5
Amina <amina@github.com>
2018-05-05 20:54:39 +0200

    Mariam Work

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { BrowserRouter, Route} from 'react-router-dom'
 import profile from './../js/profile';
 import NewsLetter from './../js/NewsLetter';
-import NewsLetterHeader from './../js/NewsLetterHeader'
-
+import AddBlog from './../js/AddBlog';
 export default ()=> (
 		<BrowserRouter>
 			<div>
 				<Route path="/NewsLetter" component ={NewsLetter}/>
 				<Route path="/profile" component={profile}/>
+				<Route path="/AddBlog" component={AddBlog}/>
 			</div>
 		</BrowserRouter>
 );
