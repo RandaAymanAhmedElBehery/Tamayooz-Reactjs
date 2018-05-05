852deb0244c7e791eed8d07131819cc9e5aa4486
sarayaser <sarayasser_110@yahoo.com>
2018-05-05 21:50:00 +0200

    blogDetails

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,17 @@ import { BrowserRouter, Route} from 'react-router-dom'
 import NewsLetter from './../js/NewsLetter';
 import NewsLetterHeader from './../js/NewsLetterHeader'
 import profile from './../js/profile';
+import into from './../js/into';
+import blogDetails from './../js/blogDetails';
+
 
 export default ()=> (
 		<BrowserRouter>
 			<div>
 				<Route path="/NewsLetter" component ={NewsLetter}/>
 				<Route path="/profile" component={profile}/>
+				<Route path="/into" component={into}/>
+				<Route path="/blogDetails" component={blogDetails}/>
 			</div>
 		</BrowserRouter>
 );
